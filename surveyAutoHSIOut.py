96098900d67ee3347403516fb7a899627e8794b9
Jesse Arzate <97304170+baileyarzate@users.noreply.github.com>
2025-01-23 15:37:36 -0800

    Add files via upload

---
+++ surveyAutoHSIOut.py
@@ -0,0 +1,9 @@
+
+
+## 1. prompt user to choose a survey type
+
+## 2. have user input the data OR select a .csv or .xlsx file
+
+## 3. do analysis on the data
+
+## 4. output the results relative to each data input type. 
